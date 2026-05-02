33c1348425959441635a5befb1937aa6104974f4
nazzzzdza <nnnarrytta@gmail.com>
2026-05-02 20:50:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot is running!");
+});
+
+app.listen(3000, () => {
+  console.log("Web server running on port 3000");
+});
+
 const {
   Client,
   GatewayIntentBits,
