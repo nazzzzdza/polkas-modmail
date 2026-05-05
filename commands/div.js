e53d42742089621c1b90a00555fc1c51b8e1d198
nazzzzdza <nnnarrytta@gmail.com>
2026-05-05 17:22:45 +0200

    Update div.js

--- commands/div.js
+++ commands/div.js
@@ -2,8 +2,12 @@ module.exports = {
   name: "div",
 
   async execute(message) {
-    await message.channel.send({
-      files: ["https://cdn.discordapp.com/attachments/1461112512534675713/1501240193611464734/image.png"]
-    });
+    // delete the command message
+    await message.delete().catch(() => {});
+
+    // send image (auto-embeds)
+    await message.channel.send(
+      "https://cdn.discordapp.com/attachments/1461112512534675713/1501240193611464734/image.png"
+    );
   }
 };
